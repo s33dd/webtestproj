3480ed7d241e33f9e7b61a0dbe009672e95482af
s33dd <dsuhoverikov@gmail.com>
2022-02-02 09:59:07 +0200

    found mistake with clicking

--- js/range_progress.js
+++ js/range_progress.js
@@ -11,23 +11,24 @@ $(document).ready(function () {
         left: 4.2
     });
     range.on('input',function (){
-        progress = (value - min)/(max - min) * 100;
         value = range.val();
+        progress = (value - min)/(max - min) * 100;
         range.css({
             background: 'linear-gradient(90deg, rgb(12, 125, 255) 0%, rgb(12, 125, 255) ' + progress
                 + '%, rgb(232, 242, 254)' + progress + '%, rgb(232, 242, 254) 100%)'
         })
         value_window.html(value);
-        if ((value != 100) && (value != 0)) {
-            if (progress < value) {
-                progress += 1;
-            }
-            if (progress > value) {
+        if ((value == 100) || (value < 10)) {
+            if (value == 100) {
                 progress -= 1;
+            } else {
+                progress += 1;
             }
         }
         value_window.css({
             left: progress * 4.2
         });
+        console.log(value);
+        console.log(progress);
     });
 });
