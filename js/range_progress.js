117816d0c3c54133dd46592976668c72b8a74b2e
s33dd <dsuhoverikov@gmail.com>
2022-02-12 18:09:00 +0200

    new ticks

--- js/range_progress.js
+++ js/range_progress.js
@@ -5,12 +5,16 @@ $(document).ready(function () {
     let min = range.attr('min');
     let progress = (value - min)/(max - min) * 100;
     let value_window = $('.range_value');
+    let rangeWidth = $('.rulerbox').width();
+    let thumbWidth = 24;
 
     value_window.html(value);
     value_window.css({
         left: 4.2
     });
     range.on('input',function (){
+        rangeWidth = $('.rulerbox').width() - thumbWidth;
+        let multiplier = rangeWidth/100;
         value = range.val();
         progress = (value - min)/(max - min) * 100;
         range.css({
@@ -18,15 +22,47 @@ $(document).ready(function () {
                 + '%, rgb(232, 242, 254)' + progress + '%, rgb(232, 242, 254) 100%)'
         })
         value_window.html(value);
-        if ((value == 100) || (value < 10)) {
-            if (value == 100) {
+        if ((value === 100) || (value < 10)) {
+            if (value === 100) {
                 progress -= 1;
             } else {
                 progress += 1;
             }
         }
+
         value_window.css({
-            left: progress * 4.2
+            left: progress * multiplier
         });
     });
+
+    let ticks = document.createElement('div')
+    let tickRange = 10;
+    let left;
+    $(ticks).addClass('ticks');
+    $(ticks).appendTo($('.rulerbox'));
+    for (let i = 0; i <= max; i += tickRange){
+        left = ((i - min)/(max-min)) * (rangeWidth - thumbWidth);
+        //left = Math.round(left);
+        if ((i == min) || (i == max)){
+            let added = $('<span/>').addClass('large');
+            added.appendTo($(ticks));
+            added.html(i);
+            added.css({left: left});
+        }
+        else {
+            if ((i % 10 === 0) && (i / 10 % 2 === 0)){
+                let added = $('<span/>');
+                added.appendTo($(ticks));
+                added.html(i);
+                left += thumbWidth/4;
+                added.css({left: left});
+            }
+            else {
+                let added = $('<span/>').addClass('tick');
+                added.appendTo($(ticks));
+                left += thumbWidth/2;
+                added.css({left: left});
+            }
+        }
+    }
 });
