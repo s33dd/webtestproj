2505529ac7e1001e971d0b47a5a5261a465c29ff
s33dd <dsuhoverikov@gmail.com>
2022-02-15 00:00:42 +0200

    small rearrangement

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+  <div class="footer__content container">
+    <div class="top-part">
+      <div class="footer__content-list footer__content-services">
+        <span>Услуги</span>
+        <a href="#">Бухгалтерский учёт</a>
+        <a href="#">Восстановление отчётности</a>
+        <a href="#">Нулевая отчётность</a>
+        <a href="#">Зарплата и кадры</a>
+        <a href="#">Консультация бухгалтера</a>
+        <a href="#">Регистрационные услуги</a>
+      </div>
+      <div class="footer__content-list footer__content-company">
+        <span>Компания</span>
+        <a href="#">О нас</a>
+        <a href="#">Тарифы</a>
+        <a href="#">Блог</a>
+        <a href="#">Контакты</a>
+        <a href="#">Политика конфиденциальности</a>
+      </div>
+      <div class="footer__content-vacancies">
+        <span>Вакансии</span>
+        <p>
+          Если Вас интересует работа в компании «НеГа Консалтинг», просим предварительно
+          направить свое резюме и предложения по заработной плате менеджеру по персоналу
+          на почту: <a href="mailto:job@negacons.ru">job@negacons.ru</a>
+        </p>
+      </div>
+    </div>
+    <div class="bottom-part">
+      <hr>
+      <div class="footer__content-end">
+        <span>© 1999-2022 ООО «НеГа Консалтинг» | Бухгалтерские услуги</span>
+        <img src="./source/icons/logo.svg">
+        <span>Разработка сайта | terexov</span>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ components/header_menu.php
@@ -0,0 +1,199 @@
+<header class="header">
+  <div class="header__content container">
+    <div class="header__content-mail">
+      <i class="header__icon icon-mail"></i>
+      <span>companymail@company.com</span>
+    </div>
+    <div class="header__content-address">
+      <i class="header__icon icon-address"></i>
+      <span>Москва, Бумажная улица, д.19</span>
+    </div>
+    <div class="header__content-worktime">
+      <i class="header__icon icon-clock"></i>
+      <span>Пн-Пт 9:00 - 18:00</span>
+    </div>
+  </div>
+</header>
+<div class="menu">
+    <nav class="menu__navigation container">
+        <a class="menu__link logo" href="#">
+            <img src="source/icons/logo.svg">
+        </a>
+        <div class="menu__navigation-links">
+            <div class="menu__dropdown">
+                        <span class="menu__link">
+                            Услуги
+                            <i class="icon-dropdown-arrow menu__dropdown-icon menu__dropdown-icon_minimized"></i>
+                            <i class="icon-dropdown-arrow-up menu__dropdown-icon menu__dropdown-icon_deployed"></i>
+                        </span>
+                <div class="menu__dropdown-wrapper">
+                    <div class="menu__dropdown-content">
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_chart">
+                                <i class="icon-chart-line"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Бухгалтерский учёт</span>
+                                <p>Сопровождение бухгалтерии и отдела кадров предприятий</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_money">
+                                <i class="icon-money"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Зарплата и кадры</span>
+                                <p>Кадровое делопроизводство с нуля, учет и начисление зарплаты</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_arrow">
+                                <i class="icon-round-arrow"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Восстановление учёта и отчётности</span>
+                                <p>Восстановим бухгалтерский учёт и отчетность в любом состоянии</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_question">
+                                <i class="icon-question"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Консультация бухгалтера</span>
+                                <p>Консультация главного бухгалтера для руководителей предприятий</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_tablet">
+                                <i class="icon-paper-tablet"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Нулевая отчётность</span>
+                                <p>Подготовим и сдадим нулевую отчетность в ИФНС и ПФР</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_check">
+                                <i class="icon-check"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Регистрация ООО</span>
+                                <p>Регистрация, внесение изменений и ликвидация ООО с внесением в ЕГРЮЛ</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="menu__link">Тарифы</a>
+            <a class="menu__link">Блог</a>
+            <a class="menu__link">О нас</a>
+            <a class="menu__link">Контакты</a>
+        </div>
+        <div class="menu__navigation-number">
+            <i class="icon-phone menu__icon"></i>
+            <a href="tel:+78005553535">8 (800) 555-35-35</a>
+        </div>
+    </nav>
+</div>
+<div class="menu__mobile">
+    <div class="menu__mobile-control container">
+        <a class="logo" href="#">
+            <img src="source/icons/logo.svg">
+        </a>
+        <div class="menu__burger">
+            <div class="burger">
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <div class="menu__mobile-links">
+        <div class="links-wrapper container">
+            <a class="menu__link">Главная</a>
+            <div class="menu__dropdown">
+                        <span class="menu__link dropdown-toggler">
+                            Услуги
+                            <i class="icon-dropdown-arrow menu__dropdown-icon"></i>
+                        </span>
+                <div class="menu__dropdown-mobile">
+                    <div class="menu__dropdown-content">
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_chart">
+                                <i class="icon-chart-line"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Бухгалтерский учёт</span>
+                                <p>Сопровождение бухгалтерии и отдела кадров предприятий</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_money">
+                                <i class="icon-money"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Зарплата и кадры</span>
+                                <p>Кадровое делопроизводство с нуля, учет и начисление зарплаты</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_arrow">
+                                <i class="icon-round-arrow"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Восстановление учёта и отчётности</span>
+                                <p>Восстановим бухгалтерский учёт и отчетность в любом состоянии</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_question">
+                                <i class="icon-question"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Консультация бухгалтера</span>
+                                <p>Консультация главного бухгалтера для руководителей предприятий</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_tablet">
+                                <i class="icon-paper-tablet"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Нулевая отчётность</span>
+                                <p>Подготовим и сдадим нулевую отчетность в ИФНС и ПФР</p>
+                            </div>
+                        </div>
+                        <div class="menu__dropdown-item">
+                            <div class="menu__dropdown-item-icon menu__dropdown-icon_check">
+                                <i class="icon-check"></i>
+                            </div>
+                            <div class="menu__dropdown-category">
+                                <span>Регистрация ООО</span>
+                                <p>Регистрация, внесение изменений и ликвидация ООО с внесением в ЕГРЮЛ</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="menu__link">Тарифы</a>
+            <a class="menu__link">Блог</a>
+            <a class="menu__link">О нас</a>
+            <a class="menu__link">Контакты</a>
+            <div class="menu__contact menu__contact_number">
+                <i class="icon-phone header__icon"></i>
+                <a href="tel:+78005553535">8 (800) 555-35-35</a>
+            </div>
+            <div class="menu__contact">
+                <i class="header__icon icon-mail"></i>
+                <span>companymail@company.com</span>
+            </div>
+            <div class="menu__contact">
+                <i class="header__icon icon-address"></i>
+                <span>Москва, Бумажная улица, д.19</span>
+            </div>
+            <div class="menu__contact">
+                <i class="header__icon icon-clock"></i>
+                <span>Пн-Пт 9:00 - 18:00</span>
+            </div>
+        </div>
+    </div>
+</div>
